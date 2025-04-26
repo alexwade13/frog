1823e5681ced56f934f1738b7f91b0965bf69686
Alex Wade <alex.wade13@gmail.com>
2025-04-25 21:59:12 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <img src="./assets/frog.jpg" alt="Frog" />
+</template>
+
+<script setup>
+// No script needed
+</script>
+
+<style scoped>
+/* No styles needed, image only */
+</style>
